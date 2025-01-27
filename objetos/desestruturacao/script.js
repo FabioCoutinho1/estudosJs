377d4213b81a59f13a1e4371fb8e358b6a2ca993
FabioCoutinho <fabiofarias549@gmail.com>
2025-01-27 19:03:35 -0300

    Update script.js

--- objetos/desestruturacao/script.js
+++ objetos/desestruturacao/script.js
@@ -19,8 +19,36 @@ const funcionario = [
   { nome: "amanda", idade: 21, salario: 1700 },
 ];
 
-funcionario.forEach((i)=>{
+// funcionario.forEach((i) => {
+//   const { nome, idade, salario } = i;
+//   console.log(nome, idade, salario);
+// });
+
+// testando em função
+function frutasDes([a, b]) {
+  console.log(a, b);
+}
+
+// aqui ja estamos recebendo os dados desestruturados nos paramentros
+// ao passar o array ao chamar a função os paramentrosja iram pegar os dados desestruturados
+
+function objsDes() {
+  funcionario.forEach((i) => {
     const { nome, idade, salario } = i;
     console.log(nome, idade, salario);
-})
+  });
+}
 
+objsDes()
+
+const pessoa = { nome: "Fabio", idade: 21 };
+
+function pessoaDes({ nome, idade }) {
+  console.log(nome, idade);
+}
+
+pessoaDes(pessoa);
+
+function funcionarioTeste([a, ...b]) {}
+
+funcionarioTeste(funcionario);
